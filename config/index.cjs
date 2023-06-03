5913305a25572bdbea8dbc0dfc254fba7e238166
Mro233 <101635380+Mro233@users.noreply.github.com>
2023-06-03 20:55:46 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oNHV06qCnzOwWnXp1Iu9_Adj7r_o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: 'BS0rPqPzlmcFRG1PJQXpvv_GxbXT_mW0ksiM-0Tcrzs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-20',
       festivals: [
