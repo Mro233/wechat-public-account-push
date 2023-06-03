137ed369d09214405878ab6b1a4d0aeb7e826abe
Mro233 <101635380+Mro233@users.noreply.github.com>
2023-06-04 00:03:48 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -142,6 +142,7 @@ const USER_CONFIG = {
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
+        { keyword: 'see_day', date: '2021-03-20' },
         { keyword: 'love_day', date: '2021-04-05' },
       ],
     },
